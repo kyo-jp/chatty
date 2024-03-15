c878c78935a4ea17f6ae386be49217901163874e
uekyo <kyo@viv.co.jp>
2024-03-14 22:02:14 -0700

    Check base

---
+++ src/services/api/auth/auth.service.js
@@ -0,0 +1,27 @@
+import axios from '@services/axios';
+
+class AuthService {
+  async signUp(body) {
+    const response = await axios.post('/signup', body);
+    return response;
+  }
+
+  async signIn(body) {
+    console.log(`body`, body);
+    const response = await axios.post('/signin', body);
+    console.log(`response`, response);
+    return response;
+  }
+
+  async forgotPassword(email) {
+    const response = await axios.post('/forgot-password', { email });
+    return response;
+  }
+
+  async resetPassword(token, body) {
+    const response = await axios.post(`/reset-password/${token}`, body);
+    return response;
+  }
+}
+
+export const authService = new AuthService();
---
+++ src/services/utils/utils.service.js
@@ -0,0 +1,54 @@
+// import { addNotification, clearNotification } from '@redux/reducers/notifications/notification.reducer';
+import { addUser, clearUser } from '@redux/reducers/user/user.reducer';
+import { avatarColors } from '@services/utils/static.data';
+import { floor, random } from 'lodash';
+import { APP_ENVIRONMENT } from '@services/axios';
+
+export class Utils {
+  static avatarColor() {
+    return avatarColors[floor(random(0.9) * avatarColors.length)];
+  }
+
+  static generateAvatar(text, backgroundColor, foregroundColor = 'white') {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+
+    canvas.width = 200;
+    canvas.height = 200;
+
+    context.fillStyle = backgroundColor;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Draw text
+    context.font = 'normal 80px sans-serif';
+    context.fillStyle = foregroundColor;
+    context.textAlign = 'center';
+    context.textBaseline = 'middle';
+    context.fillText(text, canvas.width / 2, canvas.height / 2);
+
+    return canvas.toDataURL('image/png');
+  }
+
+  static dispatchUser(result, pageReload, dispatch, setUser) {
+    pageReload(true);
+    dispatch(addUser({ token: result.data.token, profile: result.data.user }));
+    setUser(result.data.user);
+  }
+
+  static clearStore({ dispatch, deleteStorageUsername, deleteSessionPageReload, setLoggedIn }) {
+    dispatch(clearUser());
+    deleteStorageUsername();
+    deleteSessionPageReload();
+    setLoggedIn(false);
+  }
+
+  static appEnvironment() {
+    if (APP_ENVIRONMENT === 'local') {
+      return 'LOCAL';
+    } else if (APP_ENVIRONMENT === 'development') {
+      return 'DEV';
+    } else if (APP_ENVIRONMENT === 'staging') {
+      return 'STG';
+    }
+  }
+}
